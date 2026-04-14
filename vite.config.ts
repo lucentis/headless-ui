59c84df470f8be4c96131778488df55565427a78
lucentis <lucentis.dev@gmail.com>
2026-04-14 09:45:19 +0200

    made config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
+import { fileURLToPath, URL, resolve } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'headless-ui',
+      // the proper extensions will be added
+      fileName: 'headless-ui',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue', 'lodash-es'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
