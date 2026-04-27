163043af667db3eee320578d0502743208ae8205
lucentis <lucentis.dev@gmail.com>
2026-04-27 09:51:49 +0200

    core/utils: made useScrollLock to prevent scrolling on dialog, drawer etc

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -1,1 +1,2 @@
 export { useId } from './useId'
+export { useScrollLock } from './useScrollLock'
---
+++ packages/core/src/utils/useScrollLock.ts
@@ -0,0 +1,43 @@
+import { onUnmounted } from 'vue'
+import { useConfig } from '../config'
+
+export function useScrollLock() {
+  const config = useConfig()
+  let originalPaddingRight = ''
+  let originalOverflow = ''
+  let locked = false
+
+  function lock(): void {
+    if (locked) return
+
+    const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth
+
+    originalOverflow = document.body.style.overflow
+    originalPaddingRight = document.body.style.paddingRight
+
+    document.body.style.overflow = 'hidden'
+
+    if (config.scrollLock === 'padding' && scrollbarWidth > 0) {
+      const currentPadding = parseInt(window.getComputedStyle(document.body).paddingRight, 10)
+      document.body.style.paddingRight = `${currentPadding + scrollbarWidth}px`
+    }
+
+    if (config.scrollLock === 'margin' && scrollbarWidth > 0) {
+      const currentMargin = parseInt(window.getComputedStyle(document.body).marginRight, 10)
+      document.body.style.marginRight = `${currentMargin + scrollbarWidth}px`
+    }
+
+    locked = true
+  }
+
+  function unlock(): void {
+    if (!locked) return
+    document.body.style.overflow = originalOverflow
+    document.body.style.paddingRight = originalPaddingRight
+    locked = false
+  }
+
+  onUnmounted(unlock)
+
+  return { lock, unlock }
+}
